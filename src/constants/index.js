1b47851a9b07e836731e265e3e32a6dfaaf6dc24
Agailh <agailham86@gmail.com>
2023-01-02 15:12:34 +0700

    style

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "generation",
+    title: "Generation",
+  },
+  {
+    id: "channel",
+    title: "Channel",
+  },
+];
